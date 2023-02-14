bf0b1214ead0b20a0c39f72af92c70e546b128fb
NewDawn0 <92283105+NewDawn0@users.noreply.github.com>
2023-02-14 01:34:11 +0100

    added some utils and argparsing...

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "notify",
     dependencies: [
-        .package(url: "https://github.com/mtynior/ColorizeSwift.git", from: "1.6.0")
+        .package(url: "https://github.com/mtynior/ColorizeSwift.git", from: "1.6.0"),
     ],
     targets: [
         .executableTarget(
--- Sources/notify/notify.swift
+++ Sources/notify/notify.swift
@@ -34,5 +34,99 @@
 */
 
 
-// TODO: Write code
+/* Imports */
+import Foundation
+import ColorizeSwift
 
+/* Defaults */
+struct Defaults {
+    let iconPath = "~/Desktop/icon.png"
+    let title = "Notify"
+}
+/* Argument struct */
+public struct Argument {
+    var name: String
+    var desc: String
+    var short: Character
+    var value: String
+    init(name: String, desc: String) {
+        self.name = name
+        self.desc = desc
+        self.short = self.name.first!
+        self.value = ""
+    }
+}
+/* Application information */
+struct AppInfo {
+    var appname = "Notify"
+    var appversion = "1.0.0"
+    var author = "NewDawn0"
+    var copyright = "NewDawn0 2023"
+    var description = "A notification tool"
+}
+
+@main
+public class Notify {
+    public static func main() {
+        /* Variables */
+        var helpArg = Argument(name: "help", desc: "Prints the help menu")
+        var iconArg = Argument(name: "icon", desc: "Sets the icon for nofification")
+        var titleArg = Argument(name: "title", desc: "Sets the title for the notification")
+        
+        /* Check commandline arguments as a closure as to not create variables useles to the rest of the programm */
+        var checkCli = {
+            let args = CommandLine.arguments
+            if args.count == 1 {
+                print("Err".red().bold() + " :: Provide an arg\n  -> \(Utils().helpShort)")
+            }
+            for (index, arg) in args.enumerated() {
+                // Check Help arg
+                if arg == "--\(helpArg.name)" || arg == "-\(helpArg.short)" {
+                    Utils.genHelp(args: [helpArg, iconArg, titleArg])
+                // Check icon arg
+                } else if arg == "--\(iconArg.name)" || arg == "-\(iconArg.short)" {
+                    if index + 1 < args.count {
+                        iconArg.value = args[index+1]
+                    } else {
+                        Utils.eprintln(errItem: "\(args[index])", corr: "\(args[index]) <Icon path>")
+                    }
+                // Check title arg
+                } else if arg == "--\(titleArg.name)" || arg == "-\(titleArg.short)" {
+                    if index + 1 < args.count {
+                        iconArg.value = args[index+1]
+                    } else {
+                        Utils.eprintln(errItem: "\(args[index])", corr: "\(args[index]) <Title>")
+                    }
+                }
+            }
+        }
+        /* Check command line arguments */
+        checkCli()
+    }
+}
+
+/* Some useful utilites grouped together */
+public class Utils {
+    /* The try help message */
+    public private(set) var helpShort = "Try '" + "notify".blue().bold() + " --help".cyan() + "' for more information"
+    /* Prints a formatted error */
+    public static func eprintln(errItem: String, corr: String) {
+        print("Err".red().bold() + " :: Invalid item '" + errItem.red() + "' -> " + corr.green())
+        print("  -> \(Utils().helpShort)")
+        exit(1)
+    }
+    /* Generate the help menu when called using -h or --help */
+    public static func genHelp(args: Array<Argument>) {
+        let info = AppInfo()
+        print(info.appname.blue().bold() + " - \(info.description)".blue())
+        print("----------------------------".blue())
+        print("Author: ".blue() + "\(info.author)")
+        print("Version: ".blue() + "\(info.appversion)")
+        print("Copyright: ".blue() + "©\(info.copyright)\n")
+        print("Options:".blue())
+        for arg in args {
+            print("    -\(arg.short)".cyan() + "\t\t\(arg.desc)")
+            print("    --\(arg.name)".cyan() + "\t\(arg.desc)\n")
+        }
+    }
+}
