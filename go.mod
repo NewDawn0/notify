aa938aba7852dc8754737576d70192f54ded4241
NewDawn0 <newdawn.v0.0+git@gmail.com>
2024-04-20 21:22:15 +0200

    add go code

---
+++ go.mod
@@ -0,0 +1,19 @@
+module notify
+
+go 1.22.1
+
+require (
+	github.com/bhendo/go-powershell v0.0.0-20190719160123-219e7fb4e41e // indirect
+	github.com/deckarep/gosx-notifier v0.0.0-20180201035817-e127226297fb // indirect
+	github.com/duythinht/option v0.0.0-20220922154555-445d3664875e // indirect
+	github.com/gen2brain/beeep v0.0.0-20240112042604-c7bb2cd88fea // indirect
+	github.com/go-toast/toast v0.0.0-20190211030409-01e6764cf0a4 // indirect
+	github.com/godbus/dbus/v5 v5.1.0 // indirect
+	github.com/juju/errors v0.0.0-20200330140219-3fe23663418f // indirect
+	github.com/martinlindhe/inputbox v0.0.0-20210326232244-b26136a79ad0 // indirect
+	github.com/martinlindhe/notify v0.0.0-20181008203735-20632c9a275a // indirect
+	github.com/nu7hatch/gouuid v0.0.0-20131221200532-179d4d0c4d8d // indirect
+	github.com/tadvi/systray v0.0.0-20190226123456-11a2b8fa57af // indirect
+	golang.org/x/sys v0.6.0 // indirect
+	gopkg.in/toast.v1 v1.0.0-20180812000517-0a84660828b2 // indirect
+)
---
+++ main.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/gen2brain/beeep"
+	"github.com/martinlindhe/inputbox"
+	"os"
+)
+
+func main() {
+	// Flags
+	alertFlag := flag.Bool("a", false, "Show alert")
+	beepFlag := flag.Bool("b", false, "Beep")
+	dialogFlag := flag.Bool("d", false, "Show dialog")
+	notifyFlag := flag.Bool("n", false, "Show notification")
+	flag.Parse()
+
+	args := flag.Args()
+	var err error
+
+	if *notifyFlag {
+		err = notify(fillArgs(args, "-n <Title> <Body (optional)> <Icon (optional)>", 1, 3))
+	} else if *alertFlag {
+		err = alert(fillArgs(args, "-a <Title> <Body (optional)> <Icon (optional)>", 1, 3))
+	} else if *beepFlag {
+		if len(flag.Args()) > 0 {
+			perror(TooManyArgs, "-b")
+		}
+		err = beep()
+	} else if *dialogFlag {
+		got := dialog(fillArgs(args, "-d <Title> <Body (optional)> <default value (optional)>", 1, 3))
+		if got != nil {
+			fmt.Println(*got)
+			return
+		}
+	} else {
+		err = notify(fillArgs(args, "-n <Title> <Body (optional)> <Icon (optional)>", 1, 3))
+	}
+	if err != nil {
+		fmt.Fprintln(os.Stderr, fmt.Sprintf("Error: %v", err))
+		os.Exit(1)
+	}
+}
+
+// Arg parser
+const (
+	TooManyArgs   string = "Too many arguments"
+	ReqArgMissing        = "Required argument missing"
+)
+
+func perror(err string, msg string) {
+	fmt.Fprintln(os.Stderr, fmt.Sprintf("Error: %s", err))
+	fmt.Fprintln(os.Stderr, fmt.Sprintf("\nUsage:\n $ notify %s", msg))
+	os.Exit(1)
+}
+
+func fillArgs(flag []string, msg string, min int, max int) (string, string, string) {
+	if len(flag) > max {
+		perror(TooManyArgs, msg)
+	} else if len(flag) < min {
+		perror(ReqArgMissing, msg)
+	}
+	res := make([]string, 3)
+	for i, s := range flag {
+		res[i] = s
+	}
+  res[1] = "."
+	return res[0], res[1], res[2]
+}
+
+// Handler funcs
+func beep() error {
+	return beeep.Beep(beeep.DefaultFreq, beeep.DefaultDuration)
+}
+
+func notify(title string, body string, assets string) error {
+	return beeep.Notify(title, body, assets)
+}
+
+func alert(title string, body string, assets string) error {
+	return beeep.Alert(title, body, assets)
+}
+
+func dialog(title string, body string, defaultVal string) *string {
+	got, ok := inputbox.InputBox(title, body, defaultVal)
+	if ok {
+		return &got
+	}
+	return nil
+}
