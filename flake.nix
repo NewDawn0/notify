1a0c0c7dd2d7dc9afd9a677a31295b4d3f2674d8
NewDawn0 <newdawn.v0.0+git@gmail.com>
2024-12-15 20:30:38 +0100

    update flake

--- flake.nix
+++ flake.nix
@@ -15,14 +15,20 @@
         let pkgs = nixpkgs.legacyPackages.${system};
         in {
           default = pkgs.buildGoModule {
-            name = "notify";
+            pname = "notify";
+            version = "1.0.0";
             src = ./.;
             vendorHash = "sha256-pyNBOPzzJ+ZcIlGP0DP+MEbQt52XyqJJ/bo+OsmPwUk=";
-            meta = with pkgs.lib; {
-              description = "Create forms and send notifications from the CLI";
+            meta = {
+              description =
+                "A command-line utility for creating forms and sending notifications";
+              longDescription = ''
+                This tool enables users to create customizable forms and send notifications directly from the command line. It's useful for automating repetitive tasks and handling user inputs easily.
+              '';
               homepage = "https://github.com/NewDawn0/notify";
-              maintainers = with maintainers; [ "NewDawn0" ];
-              license = licenses.mit;
+              license = pkgs.lib.licenses.mit;
+              maintainers = with pkgs.lib.maintainers; [ NewDawn0 ];
+              platforms = pkgs.lib.platforms.all;
             };
           };
         });
